f52af00551e892fe3babd14849260ee3056dad4f
kritika sharma <kritikasharma2281@gmail.com>
2025-05-01 23:53:34 -0400

    add ecs

--- test/s3/main.tf
+++ test/s3/main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "ap-south-1"
 }
 module "s3_bucket" {
-  source = "git::https://github.com/kritika618/terraform_new_modules.git//.modules/aws/s3"
+  source = "git::https://github.com/kritika618/terraform_new_modules.git//.modules/aws/ecs"
 
   bucket = "my-s3-bucket-1001"
   acl    = "private"
