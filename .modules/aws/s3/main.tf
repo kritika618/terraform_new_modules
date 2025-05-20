66d7e7f5803bc86fe0e05a85b6818974d973ac64
kritika618 <kritikasharma2281@gmail.com>
2025-05-20 13:21:38 -0400

    Update main.tf

--- .modules/aws/s3/main.tf
+++ .modules/aws/s3/main.tf
@@ -2,3 +2,9 @@ module "s3_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
   version = "4.7.0"
 }
+
+resource "aws_s3_bucket" "this" {
+  bucket = var.bucket_name
+  acl    = var.acl
+  tags   = var.tags
+}
